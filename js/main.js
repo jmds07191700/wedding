d8d783ffc5d476640e0ac941bd398b732d535da4
jmds07191700 <111kitty@naver.com>
2025-03-09 01:15:41 +0900

    Initial commit

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@
 // }
 
 // typeWriter();
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.content').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-bottom':'0px'},1000);
+            }
+            
+        }); 
+    });
+});
+
